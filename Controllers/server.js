3cd82b6f99ec0e139d1454057868c28f09222104
Rawle Becerril <rawlesxm@gmail.com>
2022-11-13 18:59:33 -0500

    budget data displaying in table with blank links

--- Controllers/server.js
+++ Controllers/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 const PORT = process.env.PORT 
+const budgetData = require('../models/budget.js')
+
 
 
 
@@ -13,12 +16,18 @@ const PORT = process.env.PORT
 
 //INDEX ROUTE - GET Request
 app.get('/budgets', (req, res) => {
-
+res.render('show_index.ejs', {
+    budget: budgetData,
+    })
 })
 
 //SHOW ROUTE - GET Request
 app.get('/budgets/:index', (req, res) => {
-
+    res.render('show_budget.ejs', {
+        budget: budgetData[req.params.index],
+        index: req.params.index
+        
+    })
 })
 
 //NEW ROUTE - GET Request
@@ -29,4 +38,8 @@ app.get('/budgets/new', (req, res) => {
 //CREATE ROUTE - POST Request
 app.post('/budgets', (req, res) => {
     
+})
+
+app.listen(PORT, () => {
+    console.log(`The app is listening on Port ${PORT}`)
 })
