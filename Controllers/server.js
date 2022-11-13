79bb1ac43d2a0fb750d96162615a3dc16909cf31
Rawle Becerril <rawlesxm@gmail.com>
2022-11-13 18:06:14 -0500

    initial set up

---
+++ Controllers/server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT 
+
+
+
+
+
+///////////////
+//ROUTES
+///////////////
+
+
+//INDEX ROUTE - GET Request
+app.get('/budgets', (req, res) => {
+
+})
+
+//SHOW ROUTE - GET Request
+app.get('/budgets/:index', (req, res) => {
+
+})
+
+//NEW ROUTE - GET Request
+app.get('/budgets/new', (req, res) => {
+
+})
+
+//CREATE ROUTE - POST Request
+app.post('/budgets', (req, res) => {
+    
+})
